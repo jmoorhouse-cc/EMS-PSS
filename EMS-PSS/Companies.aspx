a136ce40d1e4c2bb66ae64dd4f19bf4ef2500d5e
pjerach <pjerach@live.com>
2015-04-19 07:44:16 -0400

    company

--- EMS-PSS/Companies.aspx
+++ EMS-PSS/Companies.aspx
@@ -3,5 +3,18 @@
     <h3>Companies</h3>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    Test maintenance content
+    <table>
+        <tr><td><asp:Label runat="server" Text="Add New Company" Font-Size="30px"></asp:Label></td></tr>
+        <tr><td>Company Name:</td>
+            <td style="width: 217px"><asp:TextBox ID="tbxCmpyName" runat="server" Width="200px" /></td>
+        </tr>
+        <tr><asp:Button ID="btnAddCmpy" runat="server" Text="Add Company" OnClick="btnAddCmpy_Click" /></tr>
+        <tr><asp:Label ID="cmpyAdditionResultLabel" runat="server" Text="" ForeColor="Red" /></tr>
+    </table>
+    <table>
+        <tr>
+            <asp:GridView ID="cmpyDisplayGrid" runat="server" ShowHeader="True" />
+        </tr>
+    </table>
+    <asp:Label ID="cmpyDisplayResultLabel" runat="server" Text="" ForeColor="Red" ></asp:Label>
 </asp:Content>
--- EMS-PSS/Companies.aspx.cs
+++ EMS-PSS/Companies.aspx.cs
@@ -4,14 +4,98 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data;              // To connect to MSSql Server
+using System.Data.SqlClient;
 namespace EMS_PSS
 {
     public partial class Maintenance : System.Web.UI.Page
     {
+        string securityLevel;
+        string userName;
+        string conString;
+        string cName = "";
+        DataTable dt;
         protected void Page_Load(object sender, EventArgs e)
         {
+            securityLevel = Session["securitylevel"].ToString();
+            userName = Session["username"].ToString();
+            conString = Session["conString"].ToString();
+            displayCompany();
+        }
 
+        private void displayCompany()
+        {
+            SqlConnection conn = new SqlConnection(conString);
+            string cmdString = "SELECT * FROM dbo.tb_Company";
+            SqlCommand cmd = new SqlCommand(cmdString, conn);
+            cmd.CommandType = CommandType.Text;
+            dt = new DataTable();
+            try
+            {
+                conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            catch (SqlException exp)
+            {
+                cmpyDisplayResultLabel.Text = "ERROR: " + exp.Message;
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            if (dt.Rows.Count == 0) cmpyDisplayResultLabel.Text += "No Result to Display";
+            else cmpyDisplayResultLabel.Text = "";
+
+            cmpyDisplayGrid.DataSource = dt;
+            cmpyDisplayGrid.DataBind();
+        }
+
+        protected void btnAddCmpy_Click(object sender, EventArgs e)
+        {
+            cName = tbxCmpyName.Text;     // username
+
+            // add user
+            SqlConnection conn = new SqlConnection(conString);
+            int queryStatus = 0;
+            string cmdstring = 
+            "INSERT INTO tb_Company VALUES ('" + cName + "');";
+
+            SqlCommand cmd = new SqlCommand(cmdstring, conn);
+            cmd.CommandType = CommandType.Text;
+            try
+            {
+                conn.Open();
+                cmd.CommandText = cmdstring;
+                queryStatus = cmd.ExecuteNonQuery();
+            }
+            catch (SqlException exp)
+            {
+                cmpyAdditionResultLabel.Text = "Duplicate Company Name; " + exp.Message;
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            if (queryStatus > 0)
+            {
+                cmpyAdditionResultLabel.Text =
+                    "Company [companyName:" + cName + "] Addition Successful";
+                tbxCmpyName.Text = "";
+                displayCompany();
+            }
+            else
+            {
+                cmpyAdditionResultLabel.Text += "Company Addition Failed";
+            }
         }
     }
 }
