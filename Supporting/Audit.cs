abdb012a3c567820b74d43043340338ea7548b03
jmoorhouse-cc <jmoorhouse-cc@conestogac.on.ca>
2015-04-19 15:35:49 -0400

    failed version of CreateAudit

--- Supporting/Audit.cs
+++ Supporting/Audit.cs
@@ -8,24 +8,28 @@ using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Data;              // To connect to MSSql Server
 using System.Data.SqlClient;
+using System.Data.SqlTypes;
 
 namespace Supporting
 {
-    class Audit
+    public class Audit
     {
         public static void CreateAudit(string conString, string changedElement, string oldVal, string newVal, string uID, string empID)
         {
-            DateTime time = DateTime.Now;
-            string dateTimeStamp = time.ToString("yyy-MM-dd hh:mm:ss");
+            //DateTime time = DateTime.Now;
+            //string format = "yyyy-MM-d" + "T" + "HH:mm:ss";
+            //string sqlTime = time.ToString(format);
+            //string goodSqlTime = "'" + sqlTime + "'";
+            //SqlDateTime sqlTime = (SqlDateTime)DateTime.Now;
 
             SqlConnection conn = new SqlConnection(conString);
             SqlCommand cmd = new SqlCommand("INSERT INTO dbo.tb_Audit VALUES (@timechanged, @empID, @userID, @oldEntry, @newEntry)", conn);
-            cmd.Parameters.AddWithValue("@timechanged", dateTimeStamp);
+
+            cmd.Parameters.AddWithValue("@timechanged", SqlDbType.DateTime);
             cmd.Parameters.AddWithValue("@empID", empID);
             cmd.Parameters.AddWithValue("@userID", uID);
             cmd.Parameters.AddWithValue("@oldEntry", oldVal);
             cmd.Parameters.AddWithValue("@newEntry", newVal);
-
             try
             {
                 conn.Open();
