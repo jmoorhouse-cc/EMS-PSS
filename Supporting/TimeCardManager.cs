dd9d2eeee792a087687e40016aaf5b87546a017a
jmoorhouse-cc <jmoorhouse-cc@conestogac.on.ca>
2015-04-19 23:12:53 -0400

    updated timecard

--- Supporting/TimeCardManager.cs
+++ Supporting/TimeCardManager.cs
@@ -15,12 +15,13 @@ namespace Supporting
         public decimal sunH, monH, tueH, wedH, thuH, friH, satH;
         public decimal sunP, monP, tueP, wedP, thuP, friP, satP;
 
-        public TimeCardManager()
+        public TimeCardManager(DateTime dateData)
         {
             timeCardDate = null;
             empID = 0;
             sunH = 0; monH = 0; tueH = 0; wedH = 0; thuH = 0; friH = 0; satH = 0;
             sunP = 0; monP = 0; tueP = 0; wedP = 0; thuP = 0; friP = 0; satP = 0;
+            DateTime firstDay = CalcMonDate(dateData);
         }
         public bool SetHours(string whichDay, decimal hours)
         {
@@ -30,14 +31,93 @@ namespace Supporting
                 switch (whichDay)
                 {
                     case "SUN":
-
+                        sunH = hours;
                         break;
                     case "MON":
-
+                        monH = hours;
+                        break;
+                    case "TUE":
+                        tueH = hours;
+                        break;
+                    case "WED":
+                        wedH = hours;
+                        break;
+                    case "THU":
+                        thuH = hours;
+                        break;
+                    case "FRI":
+                        friH = hours;
+                        break;
+                    case "SAT":
+                        satH = hours;
                         break;
                 }
             }
             return validHour;
+        }
+
+        public bool SetPieces(string whichDay, int pieces)
+        {
+            bool validPieces = (pieces > MIN_PIECES);
+            if (validPieces)
+            {
+                switch (whichDay)
+                {
+                    case "SUN":
+                        sunP = pieces;
+                        break;
+                    case "MON":
+                        monP = pieces;
+                        break;
+                    case "TUE":
+                        tueP = pieces;
+                        break;
+                    case "WED":
+                        wedP = pieces;
+                        break;
+                    case "THU":
+                        thuP = pieces;
+                        break;
+                    case "FRI":
+                        friP = pieces;
+                        break;
+                    case "SAT":
+                        satP = pieces;
+                        break;
+                }
+            }
+            return validPieces;
+        }
+
+        static public DateTime CalcMonDate(DateTime dateData)
+        {
+            DateTime dateMonday = dateData;
+
+            switch (dateData.DayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    dateData = dateMonday.AddDays(-6);
+                    break;
+                case DayOfWeek.Monday:
+                    dateData = dateMonday = dateData;
+                    break;
+                case DayOfWeek.Tuesday:
+                    dateData = dateMonday.AddDays(-1);
+                    break;
+                case DayOfWeek.Wednesday:
+                    dateData = dateMonday.AddDays(-2);
+                    break;
+                case DayOfWeek.Thursday:
+                    dateData = dateMonday.AddDays(-3);
+                    break;
+                case DayOfWeek.Friday:
+                    dateData = dateMonday.AddDays(-4);
+                    break;
+                case DayOfWeek.Saturday:
+                    dateData = dateMonday.AddDays(-5);
+                    break;
+            }
+            return dateMonday;
         }
 
     }
